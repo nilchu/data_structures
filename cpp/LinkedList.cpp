5e5762f9fce4f8e9f075850fb4bd0e876f916fc1
nilchu <19635225+nilchu@users.noreply.github.com>
2020-04-24 01:52:25 +0200

    Add push method

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -3,6 +3,18 @@
 
 namespace nilchu {
 
+    class Deque {
+        public:
+            virtual void push();
+            virtual void pop();
+    };
+
+    class Queue {
+        public:
+            virtual void add();
+            virtual void remove();
+    };
+
     class Node {
         public:
             int data;
@@ -16,12 +28,31 @@ namespace nilchu {
     };
 
 
-    class LinkedList {
+    class LinkedList: public Deque, Queue {
         public:
             Node* root;
         LinkedList() {};
         LinkedList(Node* root) {
             this->root = root;
+        }
+
+        void push(int data) {
+            Node* new_node = new Node();
+            new_node->data = data;
+            new_node->next = this->root;
+            this->root = new_node;
+        }
+
+        void pop() {
+
+        }
+
+        void add() {
+
+        }
+
+        void remove() {
+
         }
 
         void printList(Node* n) {
